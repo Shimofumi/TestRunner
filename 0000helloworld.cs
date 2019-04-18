99a411664ecc2f2d7e35955dc576790b6ab56280
Shimofumi <48890286+Shimofumi@users.noreply.github.com>
2019-04-18 15:15:32 +0900

    Helloooooooworld,0から始まるC#

---
+++ 0000helloworld.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+//環境構築の際にしたこと
+//UnityHub入れた、VS入ってた、勝った
+//Allow 'unsafe' Codeにチェックを入れます
+//Assembly Definition References のUse GUIDsにチェックを入れます
+//「Burst」「Collections」「Jobs」「Mathematics」をインスコ
+//テストコードなしでTestRunnerを走らせて死んだ
+//	https://ufcpp.net/study/csharp/
+//ここを教えてもらい今日はここまで
+
+	    Debug.Log("ちかれたよパトラッシュ");
+	           }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
