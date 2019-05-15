e0ea29bcf0f1dd8ba6f1d223f9843ff0a01e07a5
Shimofumi <48890286+Shimofumi@users.noreply.github.com>
2019-05-15 16:39:28 +0900

    Junk (#1)
    * Create RAMEN
    
    * Delete RAMEN
    
    * Create Newesttest
    
    * Delete Newesttest
    
    * Create RAAAMEN.cs
    
    * RAMEN classver test
    
    * Ramenの情報を管理して文字列で表示させたかったが敗北した、構造体ならできたがクラスではな...
    
    * ランダムにa+b=cを10個Logるやつの初手下書き

---
+++ newestTest/RandomHyouji.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+
+        public void NewTestScriptSimplePasses()
+        {   //"a+b=c"をどう表記させるか...
+            int a = UnityEngine.Random.Range(0, 256);   //ランダムな値がここで入る
+            int b = UnityEngine.Random.Range(0, 256);   //0~255
+            for (int i = 0; i < 10; i++)
+            {
+                int c = a + b;      //ここはランダムではない
+                UnityEngine.Debug.Log($"{nameof(a)} + {nameof(b)} =");
+                UnityEngine.Debug.Log(c);   //文字列結合させたいなあ
+            }
+            
+        }
+
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
