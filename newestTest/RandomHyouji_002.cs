35627d2bdb144db2ef988d0b4a5ca454c7390dc3
Shimofumi <48890286+Shimofumi@users.noreply.github.com>
2019-05-20 14:10:09 +0900

    a+b=c的ランダムDebug.Log
    UnityEngine.Randomというクラスを使って a + b = c的な計算式をランダムに10個Debug.Logしてね

---
+++ newestTest/RandomHyouji_002.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            for(int  i = 0;i<10;i++){
+            int a = UnityEngine.Random.Range(0, 256);//ランダムな値として0~255を決め打ちした
+            int b = UnityEngine.Random.Range(0, 256);
+            //int c = a+b;
+            UnityEngine.Debug.Log($"{i+1}度め！！a+b={a+b}！！");
+            //UnityEngine.Debug.Log(c);
+            }
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
